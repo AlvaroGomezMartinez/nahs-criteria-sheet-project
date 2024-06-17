7caa07a64e54bb203d2f2c0cd47905dfbb56b091
Alvaro Gomez <alvaro.gomez2011@gmail.com>
2024-06-17 16:46:57 -0500

    Created the CRUD functions

--- code.js
+++ code.js
@@ -1,3 +1,35 @@
-function myFunction() {
-  
+criteriaSheet = {
+    id: '1gaGyH312ad85wpyfH6dGbyNiS4NddqH6NvzTG6RPGPA',
+    sheet: 'ACTIVE',
+}
+
+// Create
+function createRow(data) {
+  var ss = SpreadsheetApp.openById(criteriaSheet.id);
+  var sheet = ss.getSheetByName(criteriaSheet.sheet);
+  sheet.appendRow([data.name, data.age, data.email]);
+}
+
+// Read
+function readRow(rowNumber) {
+  var ss = SpreadsheetApp.openById(criteriaSheet.id);
+  var sheet = ss.getSheetByName(criteriaSheet.sheet);
+  var range = sheet.getRange(rowNumber, 1, 1, 3);
+  var values = range.getValues();
+  return {name: values[0][0], age: values[0][1], email: values[0][2]};
+}
+
+// Update
+function updateRow(rowNumber, data) {
+  var ss = SpreadsheetApp.openById(criteriaSheet.id);
+  var sheet = ss.getSheetByName(criteriaSheet.sheet);
+  var range = sheet.getRange(rowNumber, 1, 1, 3);
+  range.setValues([[data.name, data.age, data.email]]);
+}
+
+// Delete
+function deleteRow(rowNumber) {
+  var ss = SpreadsheetApp.openById(criteriaSheet.id);
+  var sheet = ss.getSheetByName(criteriaSheet.sheet);
+  sheet.deleteRow(rowNumber);
 }
