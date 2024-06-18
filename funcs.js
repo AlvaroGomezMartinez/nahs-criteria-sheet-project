7e780b3fedaaee20e3696082c42fe8fd2a8ee75c
Alvaro Gomez <alvaro.gomez2011@gmail.com>
2024-06-18 14:54:31 -0500

    Updated the function to append the row.

--- funcs.js
+++ funcs.js
@@ -1,14 +1,28 @@
 function addNewRow(rowData) {
-  
+  if (rowData !== undefined) {
   const currentDate = new Date();
 
   const ss = SpreadsheetApp.getActiveSpreadsheet();
   const ws = ss.getSheetByName("Active");
-  ws.appendRow([rowData.name, rowData.campus, rowData.grade, rowData.id, rowData.stuOffense, currentDate, rowData.numDaysAssigned, , , , rowData.recidivist, rowData.program, rowData.specPops, rowData.behContract, rowData.notes ]);
-
+  
+  ws.appendRow([
+    rowData.name,
+    rowData.campus,
+    rowData.grade,
+    rowData.id,
+    rowData.stuOffense,
+    currentDate,
+    rowData.numDaysAssigned,
+    "",
+    "",
+    "",
+    rowData.recidivist,
+    rowData.program,
+    rowData.specPops,
+    rowData.behContract,
+    rowData.notes
+  ]);
+} else {
+  console.log("rowData is undefined");
 }
-
-function anotherFunc() {
-
-  addNewRow({})
 }
