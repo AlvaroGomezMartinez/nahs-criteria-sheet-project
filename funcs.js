e9aa6d6aa525c8dcfde21387dc4e5b38dc3414c5
Alvaro Gomez <alvaro.gomez2011@gmail.com>
2024-09-24 08:24:18 -0500

    Initial commit of these files after deleting the .gs files I had created.

---
+++ funcs.js
@@ -0,0 +1,34 @@
+/**
+ * This function loads the data that the clerk enters into the sidebar of the Google Sheets UI.
+ * 
+ * @param {*} rowData
+ * @return {*} 
+ */
+function addNewRow(rowData) {
+  if (rowData !== undefined) {
+  const currentDate = new Date().toLocaleString('en-US');
+
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const ws = ss.getSheetByName("Active");
+  ws.appendRow([
+    rowData.name,
+    rowData.campus,
+    rowData.grade,
+    rowData.id,
+    rowData.stuOffense,
+    currentDate,
+    rowData.numDaysAssigned,
+    "",
+    "",
+    "",
+    rowData.recidivist,
+    rowData.program,
+    rowData.specPops,
+    rowData.behContract,
+    rowData.notes
+  ]);
+  return true;
+  } else {
+  console.log("rowData is undefined");
+  }
+}
